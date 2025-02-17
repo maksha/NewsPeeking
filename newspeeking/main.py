86347ecdfc644d81c9406801ce53696e3c9cec8b
Muhammad Baja Aksha <maksha@gmail.com>
2025-02-17 20:17:36 +0700

    add FastAPI detail

--- newspeeking/main.py
+++ newspeeking/main.py
@@ -8,7 +8,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title="NewsPeeking API",
-              description="API to crawl news websites, extract articles, and classify them.")
+              description="API to crawl news websites, extract articles, and classify them.",
+              version="1.0.0",
+              openapi_url="/openapi.json",  # OpenAPI schema
+              docs_url="/docs",             # Swagger UI
+              redoc_url="/redoc"            # ReDoc
+              )
 
 app.include_router(api_router)
 
